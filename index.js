1db04f282dbdf473f9faffaa9545fbd281d98bfe
Lilian Carvalho <51184806+liliantavarez@users.noreply.github.com>
2022-06-07 09:39:00 -0300

    ✨ feat: recebendo dados do formulário

--- index.js
+++ index.js
@@ -1,45 +1,43 @@
 const express = require("express");
-const {engine} = require("express-handlebars")
+const { engine } = require("express-handlebars");
 const app = express();
-const bodyParser = require('body-parser')
-const Sequelize  = require("sequelize");
-const Post = require('./modelos/Post')
-
-
+const bodyParser = require("body-parser");
+const Sequelize = require("sequelize");
+const Post = require("./modelos/Post");
 
 //config
-    //template engine
-    app.engine("handlebars", engine({defaultLayout: 'main'}))
-    app.set("view engine", "handlebars")
-    app.set("views", "./views")
-    app.use('/public', express.static('public'));
+//template engine
+app.engine("handlebars", engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-    //body Parser
-    app.use(bodyParser.urlencoded({extended: false}))
-    app.use(bodyParser.json())
+app.set("views", "./views")
+app.use('/public', express.static('public'));
 
-    //conexão com mysql
-    const sequelize = new Sequelize ('heroku_d04059a677b6d56',   'b51502a7faf26a', '27699f73', {
-        host : "us-cdbr-east-05.cleardb.net",
-        dialect: 'mysql'
-    })
+//body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//conexão com mysql
+// const sequelize = new Sequelize ('heroku_d04059a677b6d56',   'b51502a7faf26a', '27699f73', {
+//     host : "us-cdbr-east-05.cleardb.net",
+//     dialect: 'mysql'
+// })
 
 //rotas
-    app.get('', function(req, res){
-        res.render('cadastro')
-    })
-    app.post('/save', function(req, res){
-        Post.create({
-            nome: req.body.nome,
-            email: req.body.email,
-            senha: req.body.senha
-        })
-    })
-    
+app.get("/", function (req, res) {
+  res.render("cadastro");
+});
 
+app.post("/save", function (req, res) { 
+    let cadastro ={
+        nome: req.body.nome,
+        email: req.body.email,
+        senha: req.body.senha
+    }
+    res.send(cadastro)
+});
 
-app.listen(8081, function(){
-    console.log("servidor rodando localhost:8081")
+app.listen(8081, function () {
+  console.log("servidor rodando localhost:8081");
 });
 //localhost:8081
-
