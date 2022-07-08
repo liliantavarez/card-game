40c2ad8e649f5c60ef022593ca66fe08e23eb716
Lilian Carvalho <51184806+liliantavarez@users.noreply.github.com>
2022-07-08 18:41:55 -0300

    ativando e desativando botões de check e cross

--- public/js/index.js
+++ public/js/index.js
@@ -14,3 +14,36 @@ function readImage() {
     }
 }
 document.getElementById("fupload").addEventListener("change", readImage, false);
+
+function ativaCross(index) {
+    document.getElementsByClassName("imgCross")[index].style.filter = "none";
+    document.getElementsByClassName("btCross")[index].disabled = false;
+}
+
+function desativaCross(index) {
+    document.getElementsByClassName("imgCross")[index].style.filter = "grayscale(100%)";
+    document.getElementsByClassName("btCross")[index].disabled = true;
+}
+
+function ativaCheck(index) {
+    document.getElementsByClassName("imgCheck")[index].style.filter = "none";
+    document.getElementsByClassName("btCheck")[index].disabled = false;
+}
+
+function desativaCheck(index) {
+    document.getElementsByClassName("imgCheck")[index].style.filter = "grayscale(100%)";
+    document.getElementsByClassName("btCheck")[index].disabled = true;
+}
+
+function onOff(id) {
+    if (id === "quantMag") {
+        desativaCross(2);
+        ativaCheck(2);
+    } else if (id === "quantDef") {
+        desativaCross(1);
+        ativaCheck(1);
+    } else if (id === "quantAtaq") {
+        desativaCross(0);
+        ativaCheck(0);
+    }
+}
