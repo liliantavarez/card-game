0faf4744c66a2cad79d928e91e7cfaddacab0e88
Lilian Carvalho <51184806+liliantavarez@users.noreply.github.com>
2022-07-08 18:26:39 -0300

    ✨ feat: exibir imagem na carta

--- public/js/index.js
+++ public/js/index.js
@@ -4,3 +4,13 @@ function onOffDeckCards() {
     document.querySelector("body").classList.toggle("hideScroll");
     document.querySelector("#deckCards").classList.toggle("addScroll");
 }
+function readImage() {
+    if (this.files && this.files[0]) {
+        const file = new FileReader();
+        file.onload = function (e) {
+            document.getElementById("preview").src = e.target.result;
+        };
+        file.readAsDataURL(this.files[0]);
+    }
+}
+document.getElementById("fupload").addEventListener("change", readImage, false);
